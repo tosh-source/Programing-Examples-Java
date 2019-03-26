6a145737790436d949b0e0a5de9d0584dbd79a8a
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-03-26 02:07:02 +0200

    add bonus example

--- src/compareMethods_equals_/compareMethods.java
+++ src/compareMethods_equals_/compareMethods.java
@@ -19,7 +19,6 @@ public class compareMethods {
         System.out.println(str1.equals(str2));  //".equals()" method compare "value" inside String instances (on the heap memory)
         System.out.println(str1 == str2);       //"==" operator compare ONLY object references
 
-
         //Ib. Compare different String objects (same value, different instance)
         String str3 = new String(str1);  //"abc", but on another position on heap memory (different instances)
 
@@ -28,7 +27,6 @@ public class compareMethods {
 
         //.equals() <- good practice (but ONLY for non-null reference values) -> https://docs.oracle.com/javase/7/docs/api/java/lang/Object.html#equals(java.lang.Object)
         //"=="      <- BAD practice (only in special cases, like null compares) -> https://docs.oracle.com/javase/tutorial/java/nutsandbolts/op2.html
-
 
         //II. Compare Integers.
         //https://www.quora.com/Can-we-compare-integers-by-using-equals-in-Java
@@ -42,6 +40,26 @@ public class compareMethods {
 
         System.out.println(NEW_LINE + int1.equals(int2));
         System.out.println((int1 == int2) + WRONG_RESULT);  //WRONG result!
-
     }
 }
+
+
+//BONUS: Good practice to compare Strings.
+//NOTE: .equals() method in Java can use only non-null reference values.
+//
+//1. String.isNullOrEmpty() like method on C#
+//=====================================
+//if (text == null || text.isEmpty()) {
+// }
+//=====================================
+//
+//2. If we want to compare ONLY with empty string, without worrying about null values.
+//NOTE: The empty string ("") should be first object. The second string object ("text"), can have string value, can be empty or null (without exception).
+//======================
+//if ("".equals(text)) {
+// }
+//======================
+//Results:
+//"" -> true
+//"some text" -> false
+//null -> false
