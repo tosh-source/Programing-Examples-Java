2f99fb4d09a273681bfb4d122eedb00138ee4a10
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-04-09 01:45:57 +0300

    add one more example in Java_time (DateTime) examples

--- src/dateTime_Methods/dateTime_Methods.java
+++ src/dateTime_Methods/dateTime_Methods.java
@@ -1,6 +1,5 @@
 package dateTime_Methods;
 
-import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -27,18 +26,26 @@ public class dateTime_Methods {
 
         //II. Formatting "java.time".
         //How to convert String to LocalDate -> https://www.mkyong.com/java8/java-8-how-to-convert-string-to-localdate/
+        //Class DateTimeFormatter -> https://docs.oracle.com/javase/8/docs/api/java/time/format/DateTimeFormatter.html
 
-        String str = "16-03-2019";  //NOTE: need to be in ISO format (see li9nk above!)
+        String str = "16-03-2019";  //NOTE: need to be in ISO format (see link above!)
 
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-            LocalDate anotherDate = LocalDate.parse(str, formatter);
+            LocalDate anotherDate = LocalDate.parse(str, formatter);                  //NOTE: The sequence of pattern "dd-MM-yyyy", actually will be REVERTED as "yyyy-MM-dd"!!
             System.out.println("\nFormat \"java.time\" " + anotherDate);
 
         } catch (DateTimeParseException timeEX) {
             System.out.printf("%s <- ERROR! Can not be parsed! \n", str);
             //throw timeEX;
         }
+
+        //IIa. Sample way to Format "java.time".
+        //NOTE: this method is actually better, because is print string as defined pattern. The example above REVERT the sequence of the defined pattern!!
+
+        LocalDate sampleDate = LocalDate.now();
+        String formattedDate = sampleDate.format(DateTimeFormatter.ofPattern("dd-MMM-yyyy"));
+        System.out.println("\nFormat \"java.time\" " + formattedDate);
 
     }
 }
