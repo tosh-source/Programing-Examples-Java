f1e7d169183cc1deaa8902de4d98856edcf18e1d
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-05-28 02:17:45 +0300

    12.1 Create an instance of the GSM class

--- src/OOP/GSM_MobilePhone_Homework/GSMCallHistoryTest/GSMCallHistoryTest.java
+++ src/OOP/GSM_MobilePhone_Homework/GSMCallHistoryTest/GSMCallHistoryTest.java
@@ -1,4 +1,16 @@
 package OOP.GSM_MobilePhone_Homework.GSMCallHistoryTest;
 
+import OOP.GSM_MobilePhone_Homework.homeworkMobilePhone.*;
+
 public class GSMCallHistoryTest {
+
+    public static void main(String[] args) {
+        //Problem 12. Call history test
+
+        //12.1 Create an instance of the GSM class.
+        GSM s8 = new GSM("Galaxy S8 Plus", "Samsung", "600$", "Ben",
+                new Battery("Samsung 3500mAh battery", 96, 27, BatteryType.LiIon),
+                new Display());
+
+    }
 }
