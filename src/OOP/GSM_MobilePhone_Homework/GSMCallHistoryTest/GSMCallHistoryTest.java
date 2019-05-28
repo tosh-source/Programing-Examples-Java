7c6f839db04a50ad87dba0228953d683f3c3f0f5
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-05-28 17:31:55 +0300

    12.3 Display the information about the calls

--- src/OOP/GSM_MobilePhone_Homework/GSMCallHistoryTest/GSMCallHistoryTest.java
+++ src/OOP/GSM_MobilePhone_Homework/GSMCallHistoryTest/GSMCallHistoryTest.java
@@ -2,6 +2,8 @@ package OOP.GSM_MobilePhone_Homework.GSMCallHistoryTest;
 
 import OOP.GSM_MobilePhone_Homework.homeworkMobilePhone.*;
 
+import java.math.BigDecimal;
+
 public class GSMCallHistoryTest {
 
     public static void main(String[] args) {
@@ -18,5 +20,18 @@ public class GSMCallHistoryTest {
         Integer clientPhoneNumber = 889977550;
 
         //set calls and their duration time//
+        for (Integer index = 0; index < countOfCalls; index++) {
+            clientPhoneNumber = clientPhoneNumber + index;                  //simple random number adding
+            allCalls[index] = new Call();
+            allCalls[index].setDialedPhoneNumber("0" + clientPhoneNumber);
+
+            BigDecimal indexToDeci = new BigDecimal(index.toString());
+            allCalls[index].setDuration(BigDecimal.TEN.add(indexToDeci.multiply(new BigDecimal("2"))));  //duration is in seconds (start from 10 seconds for first call and increase for the next one)
+        }
+
+        s8.AddCalls(allCalls);
+
+        //12.3 Display the information about the calls.
+        System.out.println(s8.CallHistoryToString());
     }
 }
