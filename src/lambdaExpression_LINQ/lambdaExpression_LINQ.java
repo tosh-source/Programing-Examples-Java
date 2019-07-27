01f211b798b0216bfad8596801a78a7d380b73ae
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2019-07-28 02:42:03 +0300

    add blank solution for lambda expression for Java

--- src/lambdaExpression_LINQ/lambdaExpression_LINQ.java
+++ src/lambdaExpression_LINQ/lambdaExpression_LINQ.java
@@ -1,4 +1,13 @@
 package lambdaExpression_LINQ;
 
 public class lambdaExpression_LINQ {
+
+    public static void main(String[] args) {
+
+
+    }
 }
+
+//orderedCall.forEach(call -> System.out.println(call));  //print with lambda expression
+
+//How to Get the Last Element of a Stream in Java? -> https://www.baeldung.com/java-stream-last-element
