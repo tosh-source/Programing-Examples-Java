53802fdf4031acb91dcb7b6fc045c7f4e45fbec2
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-11-24 01:22:40 +0200

    work on Stream IO example

--- src/stream_IO/Stream_IO.java
+++ src/stream_IO/Stream_IO.java
@@ -1,4 +1,21 @@
 package stream_IO;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;  //this package is needed for Stream input/output operations
+import java.io.UnsupportedEncodingException;
+
 public class Stream_IO {
+
+    //.I File writing (stream out)
+    PrintStream streamWriter;
+    {
+        try {
+            streamWriter = new PrintStream("testDoc.txt", "UTF-8");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
 }
