9387ec45fe4b37e8de223ce1dbd63e880adac7b4
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-11-29 00:55:07 +0200

    clean unneeded code

--- src/consoleAppExperiments/ConsoleAppExperiments.java
+++ src/consoleAppExperiments/ConsoleAppExperiments.java
@@ -11,14 +11,6 @@ public class ConsoleAppExperiments {
 
     public static void main(String[] args) {
 
-        String str = "ONE-string-SECOND-string-";
-        String[] splittedStr = str.split("ONE|SECOND");
-        System.out.println(String.join("\n", splittedStr));
 
-        for (String item : splittedStr) {
-            if (item.isBlank()){  //.isBlank() check for empty strings and whitespaces, unlike .isEmpty() who looking for empty strings only!
-
-            }
-        }
     }
 }
