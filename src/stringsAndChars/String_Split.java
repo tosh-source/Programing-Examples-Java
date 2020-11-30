579bc0f574901a53d45a01982e2b2796161691de
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-11-30 20:19:39 +0200

    add one more example

--- src/stringsAndChars/String_Split.java
+++ src/stringsAndChars/String_Split.java
@@ -11,7 +11,7 @@ public class String_Split {
         //Split string by criteria.
         //////////////////////////////////////////////////////////
         String str = "ONE-string-SECOND-string-";
-        String[] splittedStr = str.split("ONE|SECOND");  //.split() method always expect REGEX! *
+        String[] splittedStr = str.split("ONE|SECOND");  //.split() method always expect REGEX! **
         System.out.println(String.join(" / ", splittedStr));
         //////////////////////////////////////////////////////////
 
@@ -19,7 +19,7 @@ public class String_Split {
         //////////////////////////////////////////////////////////
         var tempVal = new ArrayList<>();
         for (String item : splittedStr) {
-            if (item.isBlank()){  //.isBlank() check for empty strings and whitespaces **
+            if (item.isBlank()){  //.isBlank() check for empty strings and whitespaces ***
                 //just skip current string
             } else {
                 tempVal.add(item);
@@ -29,12 +29,16 @@ public class String_Split {
         //////////////////////////////////////////////////////////
         System.out.println(String.join(" / ", splittedStr));  //NOTE: "null" may by print in console. This is bug in "join" method! In splitted string, there is no "null" as result!
 
+        //Split by whitespace
+        String text = "myString notMyStr    THIRD";
+        String[] splitStr = text.split(" ");  //or replace with more correct regex symbol: "\s" --> whitespace character: [ \t\n\x0B\f\r] **
+        System.out.println(String.join("\n", splitStr));
     }
 }
 
-//* regular-expression constructs -> https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#sum
-//* Split() String method in Java with examples -> https://www.geeksforgeeks.org/split-string-java-examples/
-//* Use String.split() with multiple delimiters -> https://stackoverflow.com/questions/5993779/use-string-split-with-multiple-delimiters
+//** regular-expression constructs -> https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#sum
+//** Split() String method in Java with examples -> https://www.geeksforgeeks.org/split-string-java-examples/
+//** Use String.split() with multiple delimiters -> https://stackoverflow.com/questions/5993779/use-string-split-with-multiple-delimiters
 
-//** .isBlank() check for empty strings and whitespaces, unlike .isEmpty() who looking for empty strings only! **
-//** more info -> https://stackoverflow.com/questions/23419087/stringutils-isblank-vs-string-isempty
+//*** .isBlank() check for empty strings and whitespaces, unlike .isEmpty() who looking for empty strings only!
+//*** more info -> https://stackoverflow.com/questions/23419087/stringutils-isblank-vs-string-isempty
