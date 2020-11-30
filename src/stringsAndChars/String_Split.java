7251a0ec4ce5485ad07bf88208bd27ac4a649cba
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-12-01 00:03:23 +0200

    add more examples

--- src/stringsAndChars/String_Split.java
+++ src/stringsAndChars/String_Split.java
@@ -10,13 +10,13 @@ public class String_Split {
 
         //Split string by criteria.
         //Use Regular expression OR "|" , to split text and catch many strings. Example --> "ONE|SECOND"
-        //////////////////////////////////////////////////////////
         String str = "ONE-string-SECOND-string-";
+        //////////////////////////////////////////////////////////
         String[] splittedStr = str.split("ONE|SECOND");  //.split() method always expect REGEX! **
-        System.out.println(String.join(" / ", splittedStr));
+        System.out.println(String.join(", ", splittedStr));
         //////////////////////////////////////////////////////////
 
-        //Remove empty strings and whitespaces from results.
+        //Remove empty entries from results.
         //////////////////////////////////////////////////////////
         var tempVal = new ArrayList<>();
         for (String item : splittedStr) {
@@ -28,18 +28,28 @@ public class String_Split {
         }
         splittedStr = tempVal.toArray(splittedStr);
         //////////////////////////////////////////////////////////
-        System.out.println(String.join(" / ", splittedStr));  //NOTE: "null" may by print in console. This is bug in "join" method! In splitted string, there is no "null" as result!
+        System.out.println(String.join(", ", splittedStr));  //NOTE: "null" may by print in console. This is bug in "join" method! In splitted string, there is no "null" as result!
 
         //Split by whitespace
         String text = "myString notMyStr    THIRD";
+        //////////////////////////////////////////////////////////
         String[] splitStr = text.split(" ");  //or replace with more correct regex symbol: "\s" --> whitespace character: [ \t\n\x0B\f\r] **
-        System.out.println(String.join("\n", splitStr));
+        //////////////////////////////////////////////////////////
+        System.out.println(String.join(", ", splitStr));
+
+        //Split by whitespace and remove empty entries from results.
+        //////////////////////////////////////////////////////////
+        String[] splitAndNoEmptyEntries = text.trim().split("\\s+");  // ****
+        //////////////////////////////////////////////////////////
+        System.out.println(String.join(", ", splitAndNoEmptyEntries));
     }
 }
 
 //** regular-expression constructs -> https://docs.oracle.com/javase/7/docs/api/java/util/regex/Pattern.html#sum
 //** Split() String method in Java with examples -> https://www.geeksforgeeks.org/split-string-java-examples/
 //** Use String.split() with multiple delimiters -> https://stackoverflow.com/questions/5993779/use-string-split-with-multiple-delimiters
+
+//**** How to split String in Java by WhiteSpace or tabs? -> https://javarevisited.blogspot.com/2016/10/how-to-split-string-in-java-by-whitespace-or-tabs.html
 
 //*** .isBlank() check for empty strings and whitespaces, unlike .isEmpty() who looking for empty strings only!
 //*** more info -> https://stackoverflow.com/questions/23419087/stringutils-isblank-vs-string-isempty
