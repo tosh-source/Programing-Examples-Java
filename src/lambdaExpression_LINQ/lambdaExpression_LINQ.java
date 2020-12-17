518da70043c2509679de385dd31c05d36ad6eba5
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2020-12-18 01:50:41 +0200

    add example

--- src/lambdaExpression_LINQ/lambdaExpression_LINQ.java
+++ src/lambdaExpression_LINQ/lambdaExpression_LINQ.java
@@ -1,9 +1,18 @@
 package lambdaExpression_LINQ;
 
+import java.util.Collections;
+
 public class lambdaExpression_LINQ {
 
     public static void main(String[] args) {
 
+        //Create 'N' elements of empty strings and convert .toArray()
+        int elements = 4;
+        ///////////////////////////////////////////////////////////
+        var result = (Collections.nCopies(elements, "value"))
+                .stream().toArray(String[]::new);
+        ///////////////////////////////////////////////////////////
+        System.out.println(String.join(System.lineSeparator(), result));
 
     }
 }
