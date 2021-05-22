393a9049fd3a110aa6797dcd176878a3476d5911
Tosh (from asus-PC-win) <alehandro_test@outlook.com>
2021-05-22 13:26:11 +0300

    add comments

--- src/OOP/reflection/Reflection.java
+++ src/OOP/reflection/Reflection.java
@@ -8,7 +8,7 @@ public class Reflection {
 
     public static final String newLine = System.lineSeparator();
 
-    public static void main(String[] args) throws IllegalAccessException {
+    public static void main(String[] args) throws IllegalAccessException {  //IllegalAccessException is needed when use get() method! **
         //Reflection is possibility to get types, methods, properties from some kind of object at runtime.
 
         //1.Use Reflection for INSTANCE of unknown class.
@@ -20,11 +20,11 @@ public class Reflection {
         objFields = cat.getClass().getDeclaredFields();     //Since in most cases fields are with private modifiers "cat.getClass().getFields()" will not work (unless they are public). So ".getDeclaredFields()" is used instead!
 
         for (var field : objFields) {
-            field.setAccessible(true);
+            field.setAccessible(true);  //Since in most cases fields are with private modifiers we need to set them accessible! NOTE: An exception WILL be thrown if fields can't be accessed!
 
             System.out.println("Private fields: "
                     + field.getName() + ": "
-                    + field.get(cat));
+                    + field.get(cat));  //To use get() we need to add exception to current main method** or surround with try/catch block!
         }
 
     }
