6b07eba003187bec57a5aafc326a3b680e5f2909
Tosh <tosh@example.com>
2021-11-05 23:16:49 +0200

    Extend Arrays Examples.

--- src/arrays/Arrays_Examples.java
+++ src/arrays/Arrays_Examples.java
@@ -4,10 +4,12 @@ import java.util.Arrays;
 
 public class Arrays_Examples {
 
+    public static String nLine = System.lineSeparator();
+
     public static void main(String[] args) {
         //1. Declare array
         int[] oneDimArr = {1, 2, 3};
-        System.out.println(oneDimArr.length + "\n");
+        System.out.printf("Arr length: %s", oneDimArr.length + nLine);
         
         //Access to array Length (row & col length)
         //IMPORTANT: Multidimensional arrays in Java, are kind of mix between "Jagged Arrays" and "Multidimensional Аrrays" in C#.
@@ -20,7 +22,7 @@ public class Arrays_Examples {
         System.out.println("Rows: " + multiDimJaggedArr.length);
         System.out.println("Cols: " + multiDimJaggedArr[0].length);
         System.out.println("Cols: " + multiDimJaggedArr[1].length);
-        System.out.println("Cols: " + multiDimJaggedArr[2].length + "\n");
+        System.out.println("Cols: " + multiDimJaggedArr[2].length + nLine);
 
         //print arrays
         //with for-each
@@ -28,14 +30,19 @@ public class Arrays_Examples {
             System.out.println(Arrays.toString(integers));
         }
         //with method "Arrays.deepToString()" <-to print "Multidimensional Аrrays"
-        System.out.println("\n" + Arrays.deepToString(multiDimJaggedArr) + "\n");
+        System.out.println("\n" + Arrays.deepToString(multiDimJaggedArr) + nLine);
         //wirh method "Arrays.toString()" <-to print normal "Array"
         String[] arrayOfStrings = {"one", "two", "three", "four"};
         System.out.println(Arrays.toString(arrayOfStrings));
 
         //Copyng two arrays
-        int[] arr4 = Arrays.copyOf(oneDimArr, oneDimArr.length);
-        System.out.println("\n" + Arrays.toString(arr4) + "\n");
+        int[] copyOfOneDimArr = Arrays.copyOf(oneDimArr, oneDimArr.length);
+        System.out.println("\n" + Arrays.toString(copyOfOneDimArr) + nLine);
+
+        //Check if arrays are really copied.
+        var testArr = copyOfOneDimArr;
+        System.out.printf("Is Equals: %s", testArr.equals(copyOfOneDimArr) + nLine);
+        System.out.printf("Is Equals: %s", copyOfOneDimArr.equals(oneDimArr) + nLine);
     }
 
 }
